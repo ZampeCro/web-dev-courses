d144fd9dad1549cd161a6cb1a8f4d87d8fd626bb
Zampe <zamparalo@gmail.com>
2022-04-19 23:13:19 +0200

    19/04 23-13

---
+++ Lecture-2 CSS-Complete guide/shared.js
@@ -0,0 +1,22 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var negativeButton = document.querySelector(".modal__action--negative");
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+});
+
+negativeButton.addEventListener("click", function () {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+});
