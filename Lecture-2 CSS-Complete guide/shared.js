356a3794c5740023c583b414fddd1437ee08537f
Zampe <zamparalo@gmail.com>
2022-04-20 20:15:08 +0200

    modify

--- Lecture-2 CSS-Complete guide/shared.js
+++ Lecture-2 CSS-Complete guide/shared.js
@@ -1,22 +1,31 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
-var negativeButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function () {
+  selectPlanButtons[i].addEventListener("click", function() {
     modal.style.display = "block";
     backdrop.style.display = "block";
   });
 }
 
-backdrop.addEventListener("click", function () {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = 'none';
+    closeModal();
 });
 
-negativeButton.addEventListener("click", function () {
-  modal.style.display = "none";
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
   backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
 });
