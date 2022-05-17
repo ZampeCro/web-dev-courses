af530a4ad2399f0d433f2abea0302d0161e40bb8
Zampe <zamparalo@gmail.com>
2022-05-17 22:44:25 +0200

    17/05

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -1,4 +1,15 @@
 // JavaScript Document
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN');
 console.log(48 + 20 + 6 - 10);
+console.log("Jonas");
+console.log("23");
+let firstName="Bob";
+console.log(firstName);
+
+
+let myCountry="BiH";
+let myContinent="Europe";
+let myPopulations="Hrvati";
+console.log(myCountry);
+console.log(myContinent);
+console.log(myPopulations);
