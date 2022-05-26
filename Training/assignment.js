7d1148d2d2d80374878614c554022e0b9da93d2d
Zampe <zamparalo@gmail.com>
2022-05-26 09:48:41 +0200

    training

---
+++ Training/assignment.js
@@ -0,0 +1,10 @@
+const customNumber = 30;
+let resuls;
+resuls = Number(18);
+const resuls1 = resuls + customNumber;
+let resuls2 = resuls1 - 20;
+resuls2 = resuls2 * 5;
+resuls2 = resuls2 / 2;
+
+alert(resuls1);
+alert(resuls2);
