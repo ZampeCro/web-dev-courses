b2cc8612e9c8a69e207d3c1cac94381abd7ff020
Zampe <zamparalo@gmail.com>
2022-05-26 15:31:01 +0200

    26/05

--- Lecture-4 JavaScript - The Complete Guide 2022/assets/scripts/app.js
+++ Lecture-4 JavaScript - The Complete Guide 2022/assets/scripts/app.js
@@ -1,8 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
 
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
+addBtn.addEventListener("click here");
 
 outputResult(currentResult, calculationDescription);
