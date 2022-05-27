35375a162699491a458f2bec26fdc1f3437ecd7c
Zampe <zamparalo@gmail.com>
2022-05-27 15:23:08 +0200

    27/05 15:23

--- Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
+++ Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
@@ -1,3 +1,19 @@
-let ninjas = ["Bozo", "Tadic", 30];
-let resuls = ninjas.join(", ");
-console.log(resuls);
+/* for (let i = 0; i < 5; i++) {
+  console.log("console in loop:", i);
+}
+console.log("loop finished");
+ */
+/* const names = ["mario", "luigi", "tom"];
+for (let i = 0; i < names.length; i++) {
+    console.log("printaj brale", i);
+ //   let html = `<div> ${names[i]} </div>`;
+//   console.log(html);
+}*/
+
+const names = ["meho", "luigi", "jovo"];
+
+let i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
