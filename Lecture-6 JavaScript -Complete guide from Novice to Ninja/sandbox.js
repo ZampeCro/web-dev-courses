2161de21df106c5b3005a40d32982de67e1a7b88
Zampe <zamparalo@gmail.com>
2022-05-30 19:21:55 +0200

    30/05/ 19:21

--- Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
+++ Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
@@ -1,20 +1,8 @@
-// const para = document.querySelector("p");
-// console.log(para.innerText);
-// para.innerText += " fighter pilot";
-// console.log(para.innerText);
+const link = document.querySelector("a");
+console.log(link.getAttribute("href"));
+link.setAttribute("href", "https://bljesak.info/");
+link.innerText = "Bljesak Mostar";
 
-// const paras = document.querySelectorAll("p");
-
-// paras.forEach((para) => {
-//   para.innerText += " karina";
-//   console.log(para.innerText);
-//
-
-//
-const content = document.querySelector(".content");
-
-const people = ["meho", "jozo", "marko"];
-
-people.forEach((person) => {
-  content.innerHTML += `<p>${person}</p>`;
-});
+const mssg = document.querySelector("p");
+console.log(mssg.getAttribute("class"));
+mssg.setAttribute("class", "success");
