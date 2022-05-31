64badc8a00d3fa4434b0a1d14ccd18837f11a1ee
Zampe <zamparalo@gmail.com>
2022-05-31 23:12:24 +0200

    31/05

--- Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
+++ Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
@@ -1,5 +1,6 @@
-const title = document.querySelector("h1");
-console.log(title.style);
-title.style.margin = "30px";
-title.style.color = "#001680";
-title.style.fontSize = "40px";
+// const content = document.querySelector("p");
+// console.log(content.classList);
+// content.classList.add("success");
+
+const paras = document.querySelectorAll("p");
+paras.
