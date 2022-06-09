26cb6a4af823045dcb57d623aa5ac49981bbd3d7
Zampe <zamparalo@gmail.com>
2022-06-09 23:37:01 +0200

    fsdfsdfd

--- Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
+++ Lecture-6 JavaScript -Complete guide from Novice to Ninja/sandbox.js
@@ -1,17 +1,11 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+const salePrices = prices.map((price) => price / 2);
+console.log(salePrices);
 
-/* const filteredScores = scores.filter((score) => {
-  return score > 20;
-});
-console.log(filteredScores);
- */
-
-const users = [
-  { name: "shaun", premium: true },
-  { name: "yoshi", premium: false },
-  { name: "mario", premium: false },
-  { name: "chun-li", premium: true },
+const products = [
+  { name: "gold star", price: 20 },
+  { name: "mushroom", price: 40 },
+  { name: "green shells", price: 30 },
+  { name: "banana skin", price: 10 },
+  { name: "red shells", price: 50 },
 ];
-const premiumUsers = users.filter((user) => user.premium);
-
-console.log(premiumUsers);
