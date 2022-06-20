129310910e18b3dce0c1721b0d6e882ce14e6cd2
Zampe <zamparalo@gmail.com>
2022-06-20 21:10:02 +0200

    Implementing the Game Logic

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+/* 
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number 🎉';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+ */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const quess = Number(document.querySelector('.guess').value);
+  console.log(quess, typeof quess);
+  if (!quess) {
+    document.querySelector('.message').textContent = '⛔ Wrong ⛔';
+  } else if (quess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number 🎉';
+  } else if (quess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 To high';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❗Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (quess < secretNumber) {
+    document.querySelector('.message').textContent = '🔺 To low';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
+  }
+});
