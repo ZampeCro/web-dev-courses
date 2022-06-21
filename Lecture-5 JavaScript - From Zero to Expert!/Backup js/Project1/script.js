cb374ed925d1f2ca520ad04a912d13811aff2be9
Zampe <zamparalo@gmail.com>
2022-06-21 15:07:36 +0200

    Project #2

---
+++ Lecture-5 JavaScript - From Zero to Expert!/Backup js/Project1/script.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/* 
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number 🎉';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+    */
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const quess = Number(document.querySelector('.guess').value);
+  console.log(quess, typeof quess);
+  //! ------ Pogresan unos------------
+  if (!quess) {
+    displayMessage('⛔ Wrong ⛔');
+    //// document.querySelector('.message').textContent = '⛔ Wrong ⛔';
+  }
+  //! ---------- BINGO ------------
+  else if (quess === secretNumber) {
+    displayMessage('Correct number 🎉');
+    //// document.querySelector('.message').textContent = 'Correct number 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+  //!------------Broj razlicit--------------
+  else if (quess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(quess > secretNumber ? '🔻 To high' : '🔻 To low');
+      /*
+       //        document.querySelector('.message').textContent =
+       //       quess > secretNumber ? '🔻 To high' : '🔻 To low';
+       */
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('❗Game Over');
+      //// document.querySelector('.message').textContent = '❗Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  /*  
+ //!-------- Broj veci--------
+  else if (quess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 To high';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❗Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //! ------- Broj manji--------
+  else if (quess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 To low';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❗Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+ */
+});
+
+/* 
+todo-------------------Coding Challenge #1------------------------
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+todo---------------------------------------------------------------
+*/
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing.....');
+  ////  document.querySelector('.message').textContent = 'Start guessing.....';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
