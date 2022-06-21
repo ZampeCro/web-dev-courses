09868aa6391d35fea5e9bf220519e3d8a2c97122
Zampe <zamparalo@gmail.com>
2022-06-21 10:35:55 +0200

    Manipulating CSS Styles

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -7,11 +7,9 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
- */
+    */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const quess = Number(document.querySelector('.guess').value);
@@ -25,6 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number 🎉';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   }
   //!-------- Broj veci--------
   else if (quess > secretNumber) {
@@ -39,8 +38,27 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //! ------- Broj manji--------
   else if (quess < secretNumber) {
-    document.querySelector('.message').textContent = '🔺 To low';
-    score = score - 1;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 To low';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❗Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+/* 
+todo-------------------Coding Challenge #1------------------------
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+todo---------------------------------------------------------------
+*/
