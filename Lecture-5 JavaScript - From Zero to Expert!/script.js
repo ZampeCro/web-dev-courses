5070716f2083db5d58e0c6afda219a3c553eb6b5
Zampe <zamparalo@gmail.com>
2022-06-22 23:15:01 +0200

    2nd Project

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -20,3 +20,12 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //// console.log('Key was pressed');
+  //// console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+  console.log('Esc was pressed');
+});
