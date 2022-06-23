0b157ff9aae0473335a6abb6abac7a65eeba46b7
Zampe <zamparalo@gmail.com>
2022-06-23 20:36:54 +0200

    switching the active player

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //!----- Selecting elements ------
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,7 +16,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //!---- Rolling dice funcionality ----
 btnRoll.addEventListener('click', function () {
@@ -29,10 +33,16 @@ btnRoll.addEventListener('click', function () {
   //todo 3.Wait for roll 1
   if (dice !== 1) {
     //* -add to current score
-    currentScore = currentScore + dice;
     ////currentScore += dice;
-    current0El.textContent = currentScore;
+    currentScore = currentScore + dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //* -switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
