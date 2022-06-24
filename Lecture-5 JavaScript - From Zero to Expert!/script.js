76eafc106bf34bccfbe3e2190e0f878f374bce02
Zampe <zamparalo@gmail.com>
2022-06-24 15:37:14 +0200

    Current score

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -16,9 +16,16 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //!---- Rolling dice funcionality ----
 btnRoll.addEventListener('click', function () {
@@ -39,10 +46,20 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //* -switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+//!----- Hold score functionality-------
+
+btnHold.addEventListener('click', function () {
+  //todo 1.Add current score to player score pool
+  ////scores[0]=scores[0]+currentScore;
+  ////scores[1]=scores[1]+currentScore;
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //todo 2.Check is player score >=100
+  //todo 3.Finish the game
+  //todo 4.Switch to next player
+  switchPlayer();
 });
