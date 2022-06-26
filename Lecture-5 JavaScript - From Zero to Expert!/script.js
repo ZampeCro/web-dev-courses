aa7f27c183f3f332a0f6a11bb5bab97c4e113c35
Zampe <zamparalo@gmail.com>
2022-06-26 22:14:57 +0200

    scoping in practice

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    let output = `Star si godina ${age}, rodjen ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1971 && birthYear <= 1996) {
+      var millenial = true;
+      //!------ Creating NEW variable with same name as outher scope variable
+      const firstName = 'Meho';
+      //!------- Reasigning outer scope variable (output)
+      output = 'Novi output';
+
+      const str = `Ferijalac iz Stoca, millenial ${firstName} `;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    console.log(output);
+    ////  console.log(add(3, 2));
+  }
+  printAge();
+  return age;
+}
+const firstName = 'Igor';
+calcAge(1975);
+
+////console.log(age);
+////printAge();
