42026f035bc885d888cbecd5dba13fdab943a7e2
Zampe <zamparalo@gmail.com>
2022-06-27 22:22:42 +0200

    hoisting

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* 
 function calcAge(birthYear) {
   const age = 2022 - birthYear;
 
@@ -9,9 +10,9 @@ function calcAge(birthYear) {
 
     if (birthYear >= 1971 && birthYear <= 1996) {
       var millenial = true;
-      //!------ Creating NEW variable with same name as outher scope variable
+      !------ Creating NEW variable with same name as outher scope variable
       const firstName = 'Meho';
-      //!------- Reasigning outer scope variable (output)
+      !------- Reasigning outer scope variable (output)
       output = 'Novi output';
 
       const str = `Ferijalac iz Stoca, millenial ${firstName} `;
@@ -23,7 +24,7 @@ function calcAge(birthYear) {
     }
     console.log(millenial);
     console.log(output);
-    ////  console.log(add(3, 2));
+    //// console.log(add(3, 2));
   }
   printAge();
   return age;
@@ -31,5 +32,50 @@ function calcAge(birthYear) {
 const firstName = 'Igor';
 calcAge(1975);
 
-////console.log(age);
-////printAge();
+//// console.log(age);
+//// printAge();
+ */
+
+//* ------ HOISTING -------
+
+//!---- Variables
+console.log(me);
+//// console.log(job);
+//// console.log(year);
+
+var me = 'Zampe';
+let job = 'tech';
+const year = 1975;
+
+//!----- Functions
+
+console.log(addDecl(2, 3));
+//// console.log(addExpr(2, 3));
+console.log(addArrow);
+//// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//!----- Example
+if (!numProduct) {
+  deleteShoppingCart();
+}
+var numProduct = 10;
+function deleteShoppingCart() {
+  console.log('Sve je izbrisano');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
