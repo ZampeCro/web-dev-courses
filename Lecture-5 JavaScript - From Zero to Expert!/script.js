c787f40133ff91edcdd914be543ed0c1c5e6917f
Zampe <zamparalo@gmail.com>
2022-06-28 21:15:12 +0200

    regular vs arrow functions

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -54,14 +54,16 @@ console.log(addDecl(2, 3));
 console.log(addArrow);
 //// console.log(addArrow(2, 3));
 
+? ---- Declared function
 function addDecl(a, b) {
   return a + b;
 }
-
+? ----- Expression function
 const addExpr = function (a, b) {
   return a + b;
 };
 
+? ----- Arrow function
 var addArrow = (a, b) => a + b;
 
 !----- Example
@@ -81,17 +83,17 @@ console.log(y === window.y);
 console.log(z === window.z);
  */
 
-console.log(this);
-
+// console.log(this);
+/* 
 const calcAge = function (birthYear) {
   console.log(2022 - birthYear);
-  console.log(this);
+  // console.log(this);
 };
 calcAge(1975);
 
 const calcAgeArrow = birthYear => {
   console.log(2022 - birthYear);
-  console.log(this);
+  // console.log(this);
 };
 calcAgeArrow(1975);
 
@@ -99,6 +101,63 @@ const jonas = {
   year: 1991,
   calcAge: function () {
     console.log(this);
+    console.log(2022 - this.year);
   },
 };
 jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+ */
+
+//* ----- REGULAR FUNCTION VS ARROW FUNCTION -----
+////var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Zampe',
+  year: 1991,
+  calcAge: function () {
+    ////console.log(this);
+    console.log(2022 - this.year);
+
+    /* 
+    ! ------- Solution 1
+    const self = this;  //self or that
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1970 && self.year <= 1996);
+      */
+    //! ---------- Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1970 && this.year <= 1996);
+      //// console.log(this.year >= 1970 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this), console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+//! -------- Arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  a + b;
+};
+addExpr(3, 7);
