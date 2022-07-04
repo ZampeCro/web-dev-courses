79d85488f5fa4b16c1ac05ff1b0140b6f2902f73
Zampe <zamparalo@gmail.com>
2022-07-04 15:01:06 +0200

    Destructuring Objects

--- Lecture-5 JavaScript - From Zero to Expert!/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/script.js
@@ -11,10 +11,78 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    address,
+    time = '20:00',
+  }) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Mahe Djikica',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({ address: 'Mahe Djikica', starterIndex: 1 });
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//! default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//! Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 47 };
+({ a, b } = obj);
+console.log(a, b);
+
+//! Nested objects
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/* 
+
+! Destructing arrays
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -27,27 +95,28 @@ console.log(arr);
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
-/* 
+
 ! substitution main <----> secondary
 const temp = main;
 main = secondary;
 secondary = temp;
 console.log(main, secondary);
- */
+ 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-//! Recive 2 values from function
+! Recive 2 values from function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, main);
 
-//! Nested descructuring
+! Nested descructuring
 const nested = [2, 3, [4, 5]];
 /* const [i, , j] = nested;
 console.log(i, j);
- */
+ 
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
 const [p = 1, r = 1, q = 1] = [2, 9];
 console.log(p, r, q);
+ */
