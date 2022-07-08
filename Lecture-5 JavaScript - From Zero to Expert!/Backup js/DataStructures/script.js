1282963a6af7235cd2986927f07e5f5769b2aaf0
Zampe <zamparalo@gmail.com>
2022-07-08 15:19:03 +0200

    Coding challenge 1

--- Lecture-5 JavaScript - From Zero to Expert!/Backup js/DataStructures/script.js
+++ Lecture-5 JavaScript - From Zero to Expert!/Backup js/DataStructures/script.js
@@ -1,11 +1,10 @@
 'use strict';
 
-/* 
-! Data needed for a later exercise
+//! Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'; 
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-! Data needed for first part of the section
+//! Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -68,8 +67,8 @@ const rest2 = {
  // rest2.numGuests = rest2.numGuests || 10;
  // rest1.numGuests ||= 10;
  // rest2.numGuests ||= 10;
- 
-! Nullish assigment operator (null or undefined)
+ */
+//! Nullish assigment operator (null or undefined)
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
 //// rest1.owner = rest1.owner && 'Anonymous';
@@ -80,7 +79,7 @@ rest2.owner &&= 'Anonymous';
 
 console.log(rest1);
 console.log(rest2);
- */
+
 /* restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -89,7 +88,7 @@ console.log(guests);
 const correctGuest = restaurant.numGuests ?? 10;
 console.log(correctGuest);
  */
-//* USE any data type, RETURN any data type, short-circuiting
+//! USE any data type, RETURN any data type, short-circuiting
 
 /* console.log('------- OR --------');
 
@@ -279,107 +278,3 @@ console.log(i, j, k);
 const [p = 1, r = 1, q = 1] = [2, 9];
 console.log(p, r, q);
  */
-
-/* 
-todo ---------------- Coding Challenge #1 -----------------------
-
-We're building a football betting app (soccer for my American friends 😅)!
-Suppose we get data from a web service about a certain game ('game' variable on
-next page). In this challenge we're gonna work with that data.
-Your tasks:
-1. Create one player array for each team (variables 'players1' and
-'players2')
-2. The first player in any player array is the goalkeeper and the others are field
-players. For Bayern Munich (team 1) create one variable ('gk') with the
-goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-field players
-3. Create an array 'allPlayers' containing all players of both teams (22
-players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-new array ('players1Final') containing all the original team1 players plus
-'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called
-'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player
-names (not an array) and prints each of them to the console, along with the
-number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which
-team is more likely to win, without using an if/else statement or the ternary
-operator.
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-Then, call the function again with players from game.scored 
-*/
-
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-
-//todo 1
-const [players1, players2] = game.players;
-console.log(players1, players2);
-
-//todo 2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-
-//todo 3
-const allplayers = [...players1, ...players2];
-console.log(allplayers);
-
-//todo 4
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
-
-//todo 5
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
-
-//todo 6
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
-////printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
-printGoals(...game.scored);
-
-//todo 7
-team1 < team2 && console.log('Team 1 je vjerovatno pobjedio');
-team1 > team2 && console.log('Team 2 je vjerovatno pobjedio');
