4d96902b7a95d488167ed2ecd9e94264c306f568
Zampe <zamparalo@gmail.com>
2022-07-11 15:43:07 +0200

    else if

---
+++ Lecture-2 The Web Developer Bootcamp 2022/Conditionals/app.js
@@ -0,0 +1,18 @@
+/* const random = Math.random();
+if (random < 0.5) {
+  console.log('manji je od 0.5');
+  console.log(random);
+}
+if (random > 0.5) {
+  console.log('veci je od 0.5');
+  console.log(random);
+}
+ */
+function isEven(num) {
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+  let broj = prompt('unesi broj');
+  if (broj == isEven) {
+    console.log('even');
+  }
+  //AND THIS LINE ↑↑↑↑↑
+}
---
+++ Lecture-2 The Web Developer Bootcamp 2022/app.js
@@ -0,0 +1,10 @@
+/* const random = Math.random();
+if (random < 0.5) {
+  console.log('manji je od 0.5');
+  console.log(random);
+}
+if (random > 0.5) {
+  console.log('veci je od 0.5');
+  console.log(random);
+}
+ */
