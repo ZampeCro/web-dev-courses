a648e1fd03ccfa2c74c126c1030c2dcfaf07241d
Zampe <zamparalo@gmail.com>
2022-07-13 20:34:27 +0200

    objects

--- Lecture-2 The Web Developer Bootcamp 2022/app.js
+++ Lecture-2 The Web Developer Bootcamp 2022/app.js
@@ -64,3 +64,44 @@ colors.splice(1, 0, 'kafena');
 ! YOUR CODE GOES BELOW THIS LINE:
 airplaneSeats[3].splice(1, 1, 'Hugo');
  */
+/* const person = {
+  firstName: 'Mick',
+  lastName: 'Jagger',
+};
+console.log(person.firstName);
+ */
+
+/* const restaurant = {
+  name: 'Ichiran Ramen',
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: 'Brooklyn',
+  state: 'NY',
+  zipcode: '11206',
+};
+
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = [
+  restaurant.address,
+  restaurant.city,
+  restaurant.state,
+  restaurant.zipcode,
+];
+console.log(fullAddress);
+
+ */
+const restaurant = {
+  name: 'Ichiran Ramen',
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: 'Brooklyn',
+  state: 'NY',
+  zipcode: '11206',
+};
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = [
+  restaurant.address,
+  restaurant.city,
+  restaurant.state,
+  restaurant.zipcode,
+];
+console.log(fullAddress);
