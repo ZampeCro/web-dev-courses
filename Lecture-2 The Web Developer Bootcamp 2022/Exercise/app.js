58f916893968c125d4515a5bd58f39cb86440a25
Zampe <zamparalo@gmail.com>
2022-07-31 19:27:27 +0200

    events

--- Lecture-2 The Web Developer Bootcamp 2022/Exercise/app.js
+++ Lecture-2 The Web Developer Bootcamp 2022/Exercise/app.js
@@ -1,1 +1,9 @@
+const hello = document.querySelector('#hello');
+const goodbye = document.querySelector('#goodbye');
 
+hello.addEventListener('click', function () {
+  console.log('hello');
+});
+goodbye.addEventListener('click', function () {
+  console.log('goodbye');
+});
--- Lecture-2 The Web Developer Bootcamp 2022/app.js
+++ Lecture-2 The Web Developer Bootcamp 2022/app.js
@@ -1,13 +1,30 @@
-const btn = document.querySelector('#v2');
-btn.onclick = function () {
+const btn2 = document.querySelector('#v2');
+btn2.onclick = function () {
   console.log('you clicked me');
   console.log('jos jednom klikno');
 };
 function onscreen() {
   console.log('aaaaaaaaaaa');
 }
-btn.onmouseenter = onscreen;
+btn2.onmouseenter = onscreen;
 
 document.querySelector('h1').onclick = () => {
   alert('You cliked me h1');
 };
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function () {
+  alert('CLICKED');
+});
+
+const twist = function () {
+  console.log('twist');
+};
+const shout = function () {
+  console.log('shout');
+};
+const tas = document.querySelector('#tas');
+
+//* tas.onclick = twist;
+
+tas.addEventListener('click', shout, { once: true });
+tas.addEventListener('click', twist);
