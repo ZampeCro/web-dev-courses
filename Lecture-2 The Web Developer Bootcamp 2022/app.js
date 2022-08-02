5ffb97b532c09f45db122342a80679ba123671fc
Zampe <zamparalo@gmail.com>
2022-08-02 14:03:31 +0200

    forms

--- Lecture-2 The Web Developer Bootcamp 2022/app.js
+++ Lecture-2 The Web Developer Bootcamp 2022/app.js
@@ -1,5 +1,11 @@
-const btn = document
-  .querySelector('button')
-  .addEventListener('click', function () {
-    console.log('CLICKED');
-  });
+const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#list');
+form.addEventListener('submit', function (s) {
+  s.preventDefault();
+  const catName = input.value;
+  const newList = document.createElement('li');
+  newList.innerText = catName;
+  list.append(newList);
+  input.value = '';
+});
